0afebf7dd050456733f5d2f10402b9cacca8dea8
Adam IMA <59504028+IIAB-prog@users.noreply.github.com>
2025-12-19 15:42:06 +0000

    Update Backend.py

--- Backend.py
+++ Backend.py
@@ -1,36 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Éditeur de Spyder
-
-Ceci est un script temporaire.
-"""
-from flask import Flask, request, jsonify
+# api.py
 import joblib
 import pandas as pd
 
-
-
-# Charger le modèle
 model = joblib.load("eligibilite_pret_model.pkl")
 
-# Initialiser Flask
-app = Flask(__name__)
-
-# Route principale
-@app.route('/')
-def home():
-    return "API Loan Eligibility est en ligne !"
-
-# Route de prédiction
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.get_json(force=True)  # récupérer JSON
-    df = pd.DataFrame([data])            # transformer en DataFrame
+def predict_loan(data: dict):
+    df = pd.DataFrame([data])
     prediction = model.predict(df)[0]
-    return jsonify({"Loan_Status": prediction})
-
-if __name__ == '__main__':
-#     app.run(debug=True)
-    import os
-    os.environ['FLASK_ENV'] = 'development'
-    app.run(debug=True, use_reloader=False)
+    return prediction
